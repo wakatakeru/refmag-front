8397908bf062266ce9029fed98f0ce3a509e3680
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-05-09 02:56:53 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
 import Papers from './Papers/Papers';
 import NewPaper from './Paper/NewPaper';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Route exact path='/papers' component={Papers} />
         <Route path='/new-paper' component={NewPaper} />
       </div >
